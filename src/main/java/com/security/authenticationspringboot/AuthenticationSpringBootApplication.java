b67c8c6b44ff4c8e255825e50ede93448d519d98
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-03 14:21:39 +0100

    feat: initial commit

---
+++ src/main/java/com/security/authenticationspringboot/AuthenticationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.security.authenticationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/authenticationspringboot/AuthenticationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.authenticationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
