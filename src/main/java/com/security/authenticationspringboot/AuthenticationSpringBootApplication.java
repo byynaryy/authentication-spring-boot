6c7fd6be0f63968d46bbe955908415f07e8c95dd
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-05 22:01:00 +0100

    feat(AuthenticationSpringBootApplication): add CrossOrigin annotation

--- src/main/java/com/security/authenticationspringboot/AuthenticationSpringBootApplication.java
+++ src/main/java/com/security/authenticationspringboot/AuthenticationSpringBootApplication.java
@@ -2,6 +2,7 @@ package com.security.authenticationspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 @RestController
 public class AuthenticationSpringBootApplication {
 
+	@CrossOrigin("http://localhost:4200")
 	@RequestMapping("/resource")
 	public Map<String,Object> home() {
 		Map<String,Object> model = new HashMap<String,Object>();
