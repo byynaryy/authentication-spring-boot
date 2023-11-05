38e852138e91c74dfb31b3fcd24cdaa6d516ea45
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-05 22:14:30 +0100

    feat(GlobalCorsConfiguration): add Configuration and EnableWebMvc annotation

--- src/main/java/com/security/authenticationspringboot/security/GlobalCorsConfiguration.java
+++ src/main/java/com/security/authenticationspringboot/security/GlobalCorsConfiguration.java
@@ -1,2 +1,10 @@
-package com.security.authenticationspringboot.security;public class GlobalCorsConfiguration {
+package com.security.authenticationspringboot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class GlobalCorsConfiguration implements WebMvcConfigurer {
 }
