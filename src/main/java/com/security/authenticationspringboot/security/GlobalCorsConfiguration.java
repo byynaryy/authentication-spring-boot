36e9abdff07e640b0316d2b37e2436ed7d53fa98
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-06 10:47:36 +0100

    feat(GlobalCorsConfiguration): add addCorsMappings function override

--- src/main/java/com/security/authenticationspringboot/security/GlobalCorsConfiguration.java
+++ src/main/java/com/security/authenticationspringboot/security/GlobalCorsConfiguration.java
@@ -1,10 +1,18 @@
 package com.security.authenticationspringboot.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class GlobalCorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
